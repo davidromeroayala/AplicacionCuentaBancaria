48bce360bac0280e735ae8c261ce4074cf93a2ff
alumno <alumno@aula1-01>
2018-11-23 19:50:12 +0100

    http://www.luciano.es/utiles/ccc.htm

--- AplicacionCuentaBancaria/src/aplicacioncuentabancaria/CuentaBancaria.java
+++ AplicacionCuentaBancaria/src/aplicacioncuentabancaria/CuentaBancaria.java
@@ -11,11 +11,45 @@ package aplicacioncuentabancaria;
  */
 public class CuentaBancaria {
    private String nombre;
+   private String cuenta;
    private String iban;
    private String codigoEntidad;
    private String codigoOficina;
    private String codigoControl;
    private String codigoInterno;
+   private int saldo;
+
+    public CuentaBancaria(String nombre, String cuenta, String iban, String codigoEntidad, String codigoOficina, String codigoControl, String codigoInterno, int saldo) {
+        this.nombre = nombre;
+        this.cuenta = cuenta;
+        this.iban = iban;
+        this.codigoEntidad = codigoEntidad;
+        this.codigoOficina = codigoOficina;
+        this.codigoControl = codigoControl;
+        this.codigoInterno = codigoInterno;
+        this.saldo = saldo;
+    }
+
+    public CuentaBancaria() {
+    nombre="";
+    cuenta="";
+    iban="";
+    codigoEntidad="";
+    codigoOficina="";
+    codigoControl="";
+    codigoInterno="";
+    saldo=0;
+    }
+    public int ingreso(int ingresar){
+    saldo+=ingresar;
+    return saldo;
+    }
+    public int retirar(int retirar){
+    saldo-=retirar;
+    return saldo;
+    }
+    
+    }
    
    
-}
+
