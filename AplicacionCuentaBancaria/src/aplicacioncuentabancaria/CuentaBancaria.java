61002b540821fed94969cebab0ed401732d44a91
alumno <alumno@aula1-01>
2018-11-23 19:21:05 +0100

    http://www.luciano.es/utiles/ccc.htm

---
+++ AplicacionCuentaBancaria/src/aplicacioncuentabancaria/CuentaBancaria.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacioncuentabancaria;
+
+/**
+ *
+ * @author DRA69
+ */
+public class CuentaBancaria {
+   private String nombre;
+   private String iban;
+   private String codigoEntidad;
+   private String codigoOficina;
+   private String codigoControl;
+   private String codigoInterno;
+   
+   
+}
